945020b44e67b3ee08bbaac193a84fcd5f877f3f
marigichun <marigichun@gmail.com>
2017-12-11 15:59:39 +0900

    java Hello 프로그램 작성

---
+++ java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	// main: mathod(메서드),프로그램의 시작점
+	// string[] args: argument, parameter
+	public static void main(String[] args) {
+		// System.out.println 메서드:괄호 안의 글자를 출력
+		System.out.println("Hello, World");
+	}
+}
---
+++ java/new 1.java
@@ -0,0 +1,2 @@
+%JAVA_HOME%\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files\Git\cmd
+C:\Program Files\Java\jdk1.8.0_152
