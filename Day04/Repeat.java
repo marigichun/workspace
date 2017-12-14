b2e066f1288107f4c273bf9b32dc5cef24716617
marigichun <marigichun@gmail.com>
2017-12-14 12:28:04 +0900

    Repeat 반복문 예제 두가지 유형

---
+++ Day04/Repeat.java
@@ -0,0 +1,16 @@
+public class Repeat {
+
+	public static void main(String[] args) {
+		//   초기값     반복값
+		for (int i = 0; i < 10; i++) {
+		//인덱스는 0부터 시작
+		System.out.println("Hello world");
+		}
+		
+		//args배열 내용을 출력
+		for (int i = 0; i <args.length; i++) {
+			System.out.println(args [i]);}		
+
+		
+	}
+}
